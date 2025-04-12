de32a33e06278aaaca85846096d37830c514beb0
markbozsoki <bozsoki.mark@icloud.com>
2025-04-13 01:30:24 +0200

    index_template_base

---
+++ templates/index.tpl.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link href="./styles/style.css" type="text/css">
+</head>
+<body>
+	<header>
+        
+    </header>
+
+    <nav>
+        <ul>
+
+        </ul>
+    </nav>
+
+    <section>
+
+    </section>
+
+    <footer>
+
+    </footer>
+    <script src="./scripts/script.js"></script>
+</body>
+</html>
