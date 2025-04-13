699cf8e42976abcfe48ae9b2edcba2e9b23f7003
markbozsoki <bozsoki.mark@icloud.com>
2025-04-13 23:54:18 +0200

    add_404_page_template

---
+++ templates/404.tpl.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+    <title>404 - Az oldal nem található</title>
+	<link rel="stylesheet" href="styles/style.css">
+
+	<style>
+        #error-container {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+        #error-content {
+            align-items: center;
+        }
+        #error-content h1 {
+            font-size: 6rem;
+            font-weight: bold;
+            margin-bottom: 1rem;
+        }
+        #error-content p {
+            font-size: 1.5rem;
+            margin-bottom: 2rem;
+        }
+        #error-content a {
+            cursor: pointer;
+        }
+    </style>
+</head>
+
+<body>
+    <div id=error-container>
+        <div id=error-content>
+            <h1>404</h1>
+            <p>A Keresett oldal nem található...</p>
+            <a href="index.php">Vissza a főoldalra</a>
+        </div>
+    </div>
+</body>
+
+</html>
