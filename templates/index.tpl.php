9900f914bec7f100b124aee5cf3c6aab4c59095d
markbozsoki <bozsoki.mark@icloud.com>
2025-04-15 00:18:58 +0200

    update_comments

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -7,10 +7,10 @@
     
     <title><?= $current_page_data['title']; ?></title>
 
-    <!-- bootstrap css -->
+    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
-    <!-- custom css -->
+    <!-- Custom CSS -->
     <link rel="stylesheet" href="./styles/style.css">
     <?php if(isset($current_page_data['style_file']) && file_exists($current_page_data['style_file'])) { ?>
     <?php echo '<link rel="stylesheet" href="' . $current_page_data['style_file'] . '">'; ?>
@@ -42,12 +42,12 @@
         <a href="https://vaszilijedc.hu/" target="_blank">vaszilijedc.hu</a>
     </footer>
 
-    <!-- bootstrap scripts -->
+    <!-- Popper and Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
 
-    <!-- custom scripts -->
+    <!-- Custom JS -->
     <script src="./scripts/script.js"></script>
     <?php if(isset($current_page_data['script_file']) && file_exists($current_page_data['script_file'])) { ?>
     <?php echo '<script src="' . $current_page_data['script_file'] . '"></script>'; ?>
