5998db9c10fce158a2f892193582f1873ab45fab
markbozsoki <bozsoki.mark@icloud.com>
2025-04-15 00:23:20 +0200

    add_bootstrap_css_to_404_template

--- templates/404.tpl.php
+++ templates/404.tpl.php
@@ -4,6 +4,11 @@
 <head>
     <meta charset="utf-8">
     <title>404 - Az oldal nem található</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <!-- Custom CSS -->
     <link rel="stylesheet" href="styles/style.css">
 
     <style>
