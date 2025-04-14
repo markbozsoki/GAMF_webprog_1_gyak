db7f22a802e008f7cb562efca8ec9df8b4635dcf
markbozsoki <bozsoki.mark@icloud.com>
2025-04-15 00:10:12 +0200

    add_bootstrap_4_js_to_index_template

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -40,6 +40,12 @@
         <a href="https://vaszilijedc.hu/" target="_blank">vaszilijedc.hu</a>
     </footer>
 
+    <!-- bootstrap scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+
+    <!-- custom scripts -->
     <script src="./scripts/script.js"></script>
     <?php if(isset($current_page_data['script_file']) && file_exists($current_page_data['script_file'])) { ?>
     <?php echo '<script src="' . $current_page_data['script_file'] . '"></script>'; ?>
