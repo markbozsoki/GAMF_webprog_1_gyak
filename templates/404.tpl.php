1eae7048ba9cd929eef716ff1748ad79b2cfcfa7
markbozsoki <bozsoki.mark@icloud.com>
2025-04-15 00:27:14 +0200

    remove_custom_404_tpl_styling

--- templates/404.tpl.php
+++ templates/404.tpl.php
@@ -12,35 +12,11 @@
 
     <!-- Custom CSS -->
     <link rel="stylesheet" href="styles/style.css">
-
-    <style>
-        #error-container {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-        }
-        #error-content {
-            align-items: center;
-        }
-        #error-content h1 {
-            font-size: 6rem;
-            font-weight: bold;
-            margin-bottom: 1rem;
-        }
-        #error-content p {
-            font-size: 1.5rem;
-            margin-bottom: 2rem;
-        }
-        #error-content a {
-            cursor: pointer;
-        }
-    </style>
 </head>
 
 <body>
-    <div id=error-container>
-        <div id=error-content>
+    <div>
+        <div>
             <h1>404</h1>
             <p>A Keresett oldal nem található...</p>
             <a href=".">Vissza a főoldalra</a>
