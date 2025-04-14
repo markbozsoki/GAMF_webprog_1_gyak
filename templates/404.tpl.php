4939d50ce26106eae2d267894b6b0dffa41bcaaf
markbozsoki <bozsoki.mark@icloud.com>
2025-04-15 00:23:55 +0200

    add_responsive_meta_tag_to_404_template

--- templates/404.tpl.php
+++ templates/404.tpl.php
@@ -3,6 +3,8 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
     <title>404 - Az oldal nem található</title>
 
     <!-- Bootstrap CSS -->
