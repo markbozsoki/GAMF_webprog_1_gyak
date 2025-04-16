ea93329b3e96ac92e9f7a8c8d4d732b2419188ee
MoSzabina <moszabina@gmail.com>
2025-04-16 14:55:27 +0200

    add_images_and_gallery_implementation

--- templates/pages/gallery.tpl.php
+++ templates/pages/gallery.tpl.php
@@ -1,1 +1,53 @@
-<p>gallery</p>
+<?php
+    include('./include/gallery/image_gallery.inc.php');    
+    $images = array();
+    $reader = opendir($DIR);
+    while (($file = readdir($reader)) !== false) {
+        if (is_file($DIR.$file)) {
+            $end = strtolower(substr($file, strlen($file)-4));
+            if (in_array($end, $TYPES)) {
+                $images[$file] = filemtime($DIR.$file);
+            }
+        }
+    }
+    closedir($reader);
+?>
+<DOCTYPE! html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Galéria</title>
+    <style >
+ 
+    </style>
+</head>
+<body>
+    <div id="gallery">
+    <?php
+    arsort($images);
+    
+    foreach($images as $file => $date)
+    {
+    ?>
+        <div class="image">
+            <a href="<?= $DIR.$file ?>">
+                <img src="<?= $DIR.$file ?>">
+            </a>            
+            <p>Név:  <?= $file; ?></p>
+            <p>Dátum:  <?= date($DATEFORM, $date); ?></p>
+        </div>
+    <?php
+    }
+    ?>
+    </div>
+<div class="gallery-upload">
+        <form action="logicals/gallery/upload.inc.php" method="post" enctype="multipart/form-data">
+        <input type="text" name="filename" placeholder="Fájl neve...">
+        <input type="text" name="filetitle" placeholder="Kép címe...">
+        <input type="text" name="filedescription" placeholder="Leírás...">
+        <input type="file" name="file">
+        <button type="submit" name="submit">Feltöltés</button>
+        </form>
+    </div>
+</body>
+</html>
