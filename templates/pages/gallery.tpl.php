6e59a70ebbaeab612c3d400f2b5aa241541e4106
MoSzabina <moszabina@gmail.com>
2025-04-16 18:40:49 +0200

    upload_function_implemented

--- templates/pages/gallery.tpl.php
+++ templates/pages/gallery.tpl.php
@@ -1,5 +1,5 @@
 <?php
-    include('./include/gallery/image_gallery.inc.php');    
+    include(__DIR__ . '/../../include/gallery/image_gallery.inc.php');    
     $images = array();
     $reader = opendir($DIR);
     while (($file = readdir($reader)) !== false) {
@@ -12,14 +12,16 @@
     }
     closedir($reader);
 ?>
-<DOCTYPE! html>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Galéria</title>
-    <style >
- 
-    </style>
+    <title><?= $current_page_data['title']; ?></title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="./styles/gallery.css">
+    <?php if(isset($current_page_data['style_file']) && file_exists($current_page_data['style_file'])) { ?>
+    <?php echo '<link rel="stylesheet" href="' . $current_page_data['style_file'] . '">'; ?>
+    <?php } ?>
 </head>
 <body>
     <div id="gallery">
@@ -41,11 +43,12 @@
     ?>
     </div>
 <div class="gallery-upload">
-        <form action="logicals/gallery/upload.inc.php" method="post" enctype="multipart/form-data">
+        <form action="include/gallery/upload.inc.php" method="post" enctype="multipart/form-data">
         <input type="text" name="filename" placeholder="Fájl neve...">
         <input type="text" name="filetitle" placeholder="Kép címe...">
         <input type="text" name="filedescription" placeholder="Leírás...">
-        <input type="file" name="file">
+        <input type="file" name="file" required>
+        <input type="hidden" name="MAX_FILE_SIZE" value="512000">
         <button type="submit" name="submit">Feltöltés</button>
         </form>
     </div>
