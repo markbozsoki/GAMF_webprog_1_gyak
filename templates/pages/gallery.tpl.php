793417dce164a33d529fd385327b51756c58c5d2
MoSzabina <moszabina@gmail.com>
2025-04-16 18:40:49 +0200

    upload_function_implemented

--- templates/pages/gallery.tpl.php
+++ templates/pages/gallery.tpl.php
@@ -7,10 +7,19 @@
             <p>Név:  <?= $file; ?></p>
             <p>Dátum: <?= date($DATEFORM, $date) ?></p>
         </div>
-    <?php endforeach; ?>
-</div>
-<?php
-if (isset($_SESSION[''])) {
-    include 'includes/gallery/form.inc.php';
-}
-?>
+    <?php
+    }
+    ?>
+    </div>
+<div class="gallery-upload">
+        <form action="include/gallery/upload.inc.php" method="post" enctype="multipart/form-data">
+        <input type="text" name="filename" placeholder="Fájl neve...">
+        <input type="text" name="filetitle" placeholder="Kép címe...">
+        <input type="text" name="filedescription" placeholder="Leírás...">
+        <input type="file" name="file" required>
+        <input type="hidden" name="MAX_FILE_SIZE" value="512000">
+        <button type="submit" name="submit">Feltöltés</button>
+        </form>
+    </div>
+</body>
+</html>
