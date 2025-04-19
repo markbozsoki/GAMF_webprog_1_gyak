24ee3ceeca71000b4eee738679d7c04778d268b7
Márk Bozsóki <83731022+markbozsoki@users.noreply.github.com>
2025-04-19 20:13:10 +0200

    1 feladat: index responsive template formating (#9)

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -22,23 +22,28 @@
         <h1><?= $current_page_data['title']; ?></h1>
     </header>
 
-    <nav>
-        <ul>
-            <?php foreach($page_datas as $page_data_key => $page_data_value) { ?>
-            <li <?= (($page_data_value == $current_page_data) ? 'class="active"' : ''); ?>> 
-                <a href="<?= ($page_data_key == '/') ? '.' : ('?page=' . $page_data_key); ?>">
-                    <?php echo $page_data_value['title']; ?>
-                </a>
-            </li>
-            <?php } ?>
-        </ul>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <?php foreach($page_datas as $page_data_key => $page_data_value) { ?>
+                <li class="nav-item<?= (($page_data_value == $current_page_data) ? ' active' : ''); ?>"> 
+                    <a class="nav-link" href="<?= ($page_data_key == '/') ? '.' : ('?page=' . $page_data_key); ?>">
+                        <?php echo $page_data_value['title']; ?>
+                    </a>
+                </li>
+                <?php } ?>
+            </ul>
+        </div>
     </nav>
 
     <section>
         <?php include($current_page_data['html_template']); ?>
     </section>
 
-    <footer>
+    <footer class="fixed-bottom text-center">
         <a href="https://vaszilijedc.hu/" target="_blank">vaszilijedc.hu</a>
     </footer>
 
