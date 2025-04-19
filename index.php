881f3528c0fc63c7c7b86296aa7be36cb0c8eee2
markbozsoki <bozsoki.mark@icloud.com>
2025-04-19 22:55:47 +0200

    update_index_php_condition_nesting

--- index.php
+++ index.php
@@ -4,21 +4,25 @@ include('config.inc.php');
 // retrieve page data by 'page' query param
 if (isset($_GET['page'])) {
     $page_data_key = $_GET['page'];
-    if (isset($page_datas[$page_data_key]) && file_exists($page_datas[$page_data_key]['html_template'])) {
-        $current_page_data = $page_datas[$page_data_key]; // retrieve requested page data
-        include('./templates/index.tpl.php');
+    if (!isset($page_datas[$page_data_key])) {
+        load_error_page($errors['400'], "unregistered page");
+        return;
     }
-    else {
-        load_error_page($errors['404']); // requested page not found
+    if(!file_exists($page_datas[$page_data_key]['html_template'])) {
+        load_error_page($errors['404'], "template could not be loaded");
+        return;
     }
+    $current_page_data = $page_datas[$page_data_key]; // retrieve requested page data
+    include('./templates/index.tpl.php');
+    return;
 }
-else {
-    $current_page_data = $page_datas['/']; // retrieve main page data (no query param)
-    if (file_exists($current_page_data['html_template'])) {
-        include('./templates/index.tpl.php');
-    }
-    else {
-        load_error_page($errors['500']); // main page could not be loaded
-    }
+
+// load main page
+$current_page_data = $page_datas['/']; // get main page data
+if (!file_exists($current_page_data['html_template'])) {
+    load_error_page($errors['500'], 'missing index template'); // main page could not be loaded
+    return;
 }
+include('./templates/index.tpl.php');
+
 ?>
