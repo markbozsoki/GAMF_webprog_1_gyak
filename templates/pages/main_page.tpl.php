9fda35108511dbb244421312ad7468284be01ea8
MoSzabina <moszabina@gmail.com>
2025-04-19 18:38:30 +0200

    mainpage_simplified

--- templates/pages/main_page.tpl.php
+++ templates/pages/main_page.tpl.php
@@ -1,4 +1,37 @@
-<?php
-include('./includes/mainpage/videodisplay.inc.php');
-include('./includes/mainpage/map.inc.php');
-?>
+
+<ul class="list-unstyled">
+<h5>Demo</h5>
+<li class="media">
+    <video width="320" height="240" controls>
+    <source src="assets/videos/showyouthecode.mp4" type="video/mp4">
+        A böngésző nem támogatja a videó lejátszását.
+    </video>
+</li>
+     
+<h5>YouTube videó</h5>
+<li class="media">
+    <div class="ratio ratio-16x9 custom-video">
+        <iframe 
+        src="https://www.youtube.com/embed/jCZuBLD_j10?si=4j1DRBFS2Nkfegf-" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="strict-origin-when-cross-origin" 
+        allowfullscreen>
+        </iframe>
+    </div>
+</li>
+
+<h5>Elérhetőség térképen</h5>
+<li class="media">
+    <div class="ratio ratio-16x9 custom-map">
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2701.575444420692!2d19.222816375448705!3d47.38120417116998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741eb8ed25e9983%3A0x1001b036eff20959!2sVencel%20Bal%C3%A1zs%20E.V.!5e0!3m2!1shu!2hu!4v1744972642054!5m2!1shu!2hu"
+            class="w-100 rounded"
+            allowfullscreen
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+    </div>
+</li>
+</ul>
