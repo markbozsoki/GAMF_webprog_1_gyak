88e0fee3e1bb5a20ce0cfd9ebd3354df07d94bbe
markbozsoki <bozsoki.mark@icloud.com>
2025-04-20 03:05:40 +0200

    set_user_login_session

--- includes/session.inc.php
+++ includes/session.inc.php
@@ -6,8 +6,14 @@ function logout_user() {
     unset($_SESSION["logged_in"]);
 }
 
+function set_user_login_session($surname, $forename, $username, $logged_in = TRUE) {
+    $_SESSION["surname"] = $surname;
+    $_SESSION["forename"] = $forename;
+    $_SESSION["username"] = $username;
+    $_SESSION['logged_in'] = $logged_in;
+}
+
 function is_user_logged_in() {
     return isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == TRUE;
 }
-
 ?>
