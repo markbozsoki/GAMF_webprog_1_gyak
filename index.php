2a06b6456fa51ed2ec201cf16b6d2e136b97bd6c
markbozsoki <bozsoki.mark@icloud.com>
2025-04-24 01:03:33 +0200

    apply_strict_type_checks_at_the_real_entry_point

--- index.php
+++ index.php
@@ -1,44 +1,4 @@
-<?php declare(strict_types=1);
-include('config.inc.php');
-
-function load_page($page_data_key, $extra_headers = NULL) {
-    global $page_datas;
-    global $errors;
-
-    if (!isset($page_datas[$page_data_key])) {
-        load_error_page($errors['400'], "unregistered page");
-        return;
-    }
-    if(!file_exists($page_datas[$page_data_key]['html_template'])) {
-        load_error_page($errors['404'], "template could not be loaded");
-        return;
-    }
-
-    $current_page_data = $page_datas[$page_data_key]; // retrieve requested page data
-    
-    if ($extra_headers === NULL) {
-        $extra_headers = array();
-    }
-    array_push($extra_headers, create_custom_header('Loaded-Page', $page_data_key));
-    foreach($extra_headers as $extra_header) {
-        header(load_string_from_custom_header($extra_header));
-    }
-    include('./templates/index.tpl.php');
-    exit();
-}
-
-function reload_login_page($extra_headers = NULL) {
-    load_page('login', $extra_headers);
-}
-
-function redirect_to($path) {
-    header('Location: ' . $path);
-    exit();
-}
-
-function redirect_to_main_page() {
-    redirect_to('.');
-}
+<?php include('config.inc.php');
 
 function create_custom_header($key, $value) {
     global $_HEADER_PREFIX;
@@ -204,9 +164,5 @@ if (isset($_GET['page'])) {
 }
 
 // no query param were set, or fall through
-$current_page_data = $page_datas['/']; // get main page data
-if (!file_exists($current_page_data['html_template'])) {
-    load_error_page($errors['500'], 'missing index template'); // main page could not be loaded
-}
-include('./templates/index.tpl.php');
+load_main_page();
 ?>
