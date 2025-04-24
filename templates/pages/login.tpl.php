c55077daa732fcd37cf08624007eb0d81c6abd8d
markbozsoki <bozsoki.mark@icloud.com>
2025-04-24 02:09:04 +0200

    add_ids_for_forms

--- templates/pages/login.tpl.php
+++ templates/pages/login.tpl.php
@@ -1,6 +1,6 @@
 <div class="d-flex align-content-stretch flex-wrap align-items-top justify-content-center">
     <div class="mx-5 my-2 align-items-center justify-content-center">
-        <form action="?page=login&amp;login" method="POST">
+        <form id="loginForm" action="?page=login&amp;login" method="POST">
             <fieldset>
                 <legend>Bejlentkezés</legend>
                 <input class="p-1 m-1" required type="text" name="username" placeholder="felhasználónév" autocomplete="on"><br>
@@ -11,7 +11,7 @@
     </div>
 
     <div class="mx-5 my-2 align-items-center justify-content-center">
-        <form action="?page=login&amp;register" method="POST">
+        <form id="registrationForm" action="?page=login&amp;register" method="POST">
             <fieldset>
                 <legend>Regisztráció</legend>
                 <input class="p-1 m-1" required type="text" name="surname" placeholder="vezetéknév"><br>
