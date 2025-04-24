2dfbb6dc3363af4d3bd60a2883ca9d24be7f68c1
markbozsoki <bozsoki.mark@icloud.com>
2025-04-24 02:11:27 +0200

    rename_user_mgt_to_login

--- config.inc.php
+++ config.inc.php
@@ -11,7 +11,7 @@ include('./includes/data_access_layer.inc.php');
 
 // feature level includes
 include('./includes/image_gallery.inc.php');
-include('./includes/user_mgt.inc.php');
+include('./includes/login.inc.php');
 
 
 // collection of available page templates, styles and page spacific values
