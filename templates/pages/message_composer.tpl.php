ec654be1349c223405bb5ab1666dae1cb02023d1
markbozsoki <bozsoki.mark@icloud.com>
2025-04-26 17:27:45 +0200

    message_composer_html_base

--- templates/pages/message_composer.tpl.php
+++ templates/pages/message_composer.tpl.php
@@ -1,1 +1,13 @@
 <p>Üzenet küldés</p>
+
+<div>
+    <form id="messageComposerForm" action="?page=messaging" method="POST">
+        <div>
+            <input type="hidden" name="username" value=""><br>
+            <input required type="text" name="name" placeholder="teljes név" values=""><br>
+            <input required type="email" name="email" placeholder="email cím"><br>
+            <textarea required name="message" placeholder="üzenet..." cols=50 rows=20></textarea><br>
+            <input type="submit" name="send" value="Küldés">
+        </div>
+    </form>
+</div>
