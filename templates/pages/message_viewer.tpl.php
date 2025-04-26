194ef9e6fc2535a71908d89044213d63ac44732c
markbozsoki <bozsoki.mark@icloud.com>
2025-04-26 20:20:15 +0200

    update_msg_templates_with_static_content

--- templates/pages/message_viewer.tpl.php
+++ templates/pages/message_viewer.tpl.php
@@ -1,1 +1,22 @@
 <p>Egy üzenet megjelenítése</p>
+
+<div>
+    <!-- TODO: csak akkor jelenjen meg, ha új üzenet lett küldve -->
+    <h1>Köszönjük, hogy felvette velünk a kapcsolatot!</h1>
+    <p>Amint feldolgoztuk üzenetét, az ön által megadott e-mail címen fogjuk keresni.</p>
+</div>
+
+<div>
+    <h2>Test Subject</h2>
+    <div>
+        <p>2025-04-26 17:26:46</p>
+        <p>Guest</p>
+        <p>From: <a href="mailto:guest.user@example.com"></a>guest.user@example.com</p>
+    </div>
+    <p>
+        Hello, is your frigde running?\n\n\nBye
+    </p>
+</div>
+
+<a href="?page=messaging">Vissza</a> <!-- TODO: dinamikusan változó link legyen -->
+<a href=".">Vissza a főoldalra</a>
--- templates/pages/messages.tpl.php
+++ templates/pages/messages.tpl.php
@@ -1,1 +1,34 @@
 <p>Üzenetek listázása</p>
+
+<div>
+    <a href="?page=messages&start=0&size=10">előző oldal</a>
+    <a href="?page=messages&start=10&size=10">következő oldal</a>
+</div>
+
+<div>
+    <table>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Küldve</th>
+                <th>Küldő</th>
+                <th>Email</th>
+                <th>Tárgy</th>
+                <th>Üzenet</th>
+                <th>Id</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <th>1</th>
+                <th>2025-04-26 17:26:46</th>
+                <th>Guest</th>
+                <th>guest.user@example.com</th>
+                <th>Test Subject</th>
+                <th>Hello, is your frigde running?\n\n\nBye</th>
+                <th><a href="?message=msg65a8262b5a826">Megtekintés</a></th>
+            </tr>
+        </tbody>
+    </table>
+</div>
