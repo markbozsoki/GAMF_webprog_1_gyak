de9c7bc4475857dc530c69d8626e4cd99bcc4451
MoSzabina <moszabina@gmail.com>
2025-04-26 11:16:13 +0200

    1. feladat főoldal átvétele

--- templates/pages/main_page.tpl.php
+++ templates/pages/main_page.tpl.php
@@ -1,33 +1,306 @@
-<p>main page</p>
+<div class="container">
 
-<div class="d-flex align-items-center justify-content-center">
-    <div>
-        <h5>Videó saját könyvtárból</h5>
-        <div class="media">
-            <video width="300" height="170" controls>
-                <source src="assets/videos/showyouthecode.mp4" type="video/mp4">
-                A böngésző nem támogatja a videó lejátszását.
-            </video>
+    <div class="row align-items-center mb-5">
+        <div class="col-md-6">
+            <h2>Naplónak indult.</h2>
+            <h3>Bemutató bloggá vált.</h3>
+            <p class="text-justify">Aztán átalakult valami mássá. Ablakká, amelyben kitekintek a világra, a világ
+                meg betekinthet a gondolataimba: késekről, every day carry felszerelésekről,
+                és az ezek mögött meghúzódó filozófiáról.</p>
+            <p class="text-justify">Aztán ennél is több lett. Egy közösség, amelyben együtt, hasonló értékek
+                mentén dolgozunk azért, hogy egy minőségi, kissé talán régimódi találkahely
+                legyen ez az online térben.</p>
+            <p class="text-justify">Balogh József vagyok, és azon dolgozom, hogy ez a közösség egyre nagyobbá
+                váljon, és együtt adhassuk tovább ezek az értékeket. Tarts velünk te is!</p>
+        </div>
+        <div class="col-md-6">
+            <img class="rounded mx-auto d-block" style="max-width: 60%; height: auto;"
+                src="https://vaszilijedc.hu/wp-content/uploads/2018/06/IMG_20180614_163340_1.jpg.webp" alt="Vaszilij"
+                title="Vaszilij" />
+        </div>
+    </div>
+
+    <div class="mb-5">
+        <div id=carouselCaptions class="carousel slide carousel-fade" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselCaptions" data-bs-slide-to="0" class="active"
+                    aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselCaptions" data-bs-slide-to="1"
+                    aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselCaptions" data-bs-slide-to="2"
+                    aria-label="Slide 3"></button>
+            </div>
+
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="https://vaszilijedc.hu/wp-content/uploads/2025/04/20250222_1659167002.jpg"
+                        class="d-block w-100">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h2><a href="https://vaszilijedc.hu/lali-szamurajkardja/">Lali szamurájkardja</a></h2>
+                        <p>Szerző: <span><a href="https://vaszilijedc.hu/author/basaja/"
+                                    title="VaszilijEDC Ultra bejegyzései" rel="author">VaszilijEDC Ultra</a></span> |
+                            <span class="published">2025.04.06.</span> | <a
+                                href="https://vaszilijedc.hu/category/irasok/egyeb/" rel="category tag">Egyéb</a> | 0
+                            Hozzászólások
+                        </p>
+                        <p>Képzeljük el Lalit, aki nyílászárók beszerelésével
+                            keresi a kenyerét. Egyszerű építőipari munkás, aki már
+                            évek óta megbízható munkát végez. Szépen építgeti az
+                            életét, komoly barátnője van, saját lakásra gyűjt. Egyik
+                            reggel, amikor egy újépítésű házhoz szállnak ki a...</p>
+                        <div><a class="btn btn-outline-light"
+                                href="https://vaszilijedc.hu/lali-szamurajkardja/">Bővebben</a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="carousel-item">
+                    <img src="https://vaszilijedc.hu/wp-content/uploads/2025/04/IMG_2549.jpeg" class="d-block w-100">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h2><a href="https://vaszilijedc.hu/we-banter-ujratoltve/">We Banter -Újratöltve</a></h2>
+                        <p>Szerző: <span><a href="https://vaszilijedc.hu/author/basaja/"
+                                    title="VaszilijEDC Ultra bejegyzései" rel="author">VaszilijEDC Ultra</a></span> |
+                            <span class="published">2025.04.20.</span> | <a
+                                href="https://vaszilijedc.hu/category/we-knives-2/" rel="category tag">We knives</a>, <a
+                                href="https://vaszilijedc.hu/category/irasok/kesek/weknives/" rel="category tag">We
+                                knives</a>, <a href="https://vaszilijedc.hu/category/irasok/kesek/we-knives/"
+                                rel="category tag">WE Knives</a>, <a
+                                href="https://vaszilijedc.hu/category/irasok/kesek/folder/we-knives-folder/"
+                                rel="category tag">WE Knives</a> | 0 Hozzászólások
+                        </p>
+                        <p>4 évvel ezelőtt született egy cikk Ben Petersen első
+                            saját tervezésű késéről ( We Banter ). Most, hogy
+                            megjelent a második generáció, valamint hogy lassan
+                            másfél éve használok egy módosított verziót az első
+                            szériából, elegendő oknak bizonyult, hogy újra górcső
+                            alá...</p>
+                        <div><a class="btn btn-outline-light"
+                                href="https://vaszilijedc.hu/we-banter-ujratoltve/">Bővebben</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="carousel-item">
+                    <img src="https://vaszilijedc.hu/wp-content/uploads/2025/01/Kep1.jpg" class="d-block w-100">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h2><a href="https://vaszilijedc.hu/svord-peasant/">Svörd Peasant</a></h2>
+                        <p>Szerző: <span><a href="https://vaszilijedc.hu/author/basaja/"
+                                    title="VaszilijEDC Ultra bejegyzései" rel="author">VaszilijEDC Ultra</a></span> |
+                            <span class="published">2025.01.26.</span> | <a
+                                href="https://vaszilijedc.hu/category/irasok/egyeb/" rel="category tag">Egyéb</a>| 0
+                            Hozzászólások
+                        </p>
+                        <p>Magányos túrázó sétál a Fekete-Körös gátján a
+                            táborhelye felé. Apró hobó kályhájában ropog a tűz. Sült
+                            szalonna és lilahagyma illata terjeng a táborhelyen. A
+                            túrázó fatányéron katonázza a falatokat surlódó záras
+                            bicskájával. Legalábbis ez a kép jelent meg a szemeim...</p>
+                        <div><a class="btn btn-outline-light"
+                                href="https://vaszilijedc.hu/we-banter-ujratoltve/">Bővebben</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Előző</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Következő</span>
+            </button>
         </div>
 
-        <h5>YouTube videó</h5>
-        <div class="media">
-            <div class="ratio ratio-16x9 custom-video">
-                <iframe src="https://www.youtube.com/embed/jCZuBLD_j10?si=4j1DRBFS2Nkfegf-" 
-                    title="YouTube video player" frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
-                </iframe>
+    </div>
+
+    <div class="row text-center mb-5">
+        <div class="col-md-4">
+            <div class="card text-white bg-dark mb-3">
+                <h2>Facebook</h2>
+                <div>
+                    <p>Vaszilij EDC a Facebookon.</p>
+                </div>
+                <a class="btn btn-outline-light" href="https://www.facebook.com/VaszilijEdc" target="_blank"
+                    rel="noopener">Megnézem</a>
             </div>
         </div>
-
-        <h5>Elérhetőség térképen</h5>
-        <div class="media">
-            <div class="ratio ratio-16x9 custom-map">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2701.575444420692!2d19.222816375448705!3d47.38120417116998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741eb8ed25e9983%3A0x1001b036eff20959!2sVencel%20Bal%C3%A1zs%20E.V.!5e0!3m2!1shu!2hu!4v1744972642054!5m2!1shu!2hu"
-                    class="w-100 rounded" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade">
-                </iframe>
+        <div class="col-md-4">
+            <div class="card text-white bg-dark mb-3">
+                <h2>Közösség</h2>
+                <div>
+                    <p>Ahol te adod a tartalmat.</p>
+                </div>
+                <a class="btn btn-outline-light" href="https://www.facebook.com/groups/434204628313605" target="_blank"
+                    rel="noopener">Megnézem</a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card text-white bg-dark mb-3">
+                <h2>Üzenet</h2>
+                <div>
+                    <p>Küldj üzenetet nekem!</p>
+                </div>
+                <a class="btn btn-outline-light" href="https://vaszilijedc.hu/uzenetkuldes/" target="_blank"
+                    rel="noopener">Megnézem</a>
             </div>
         </div>
     </div>
-</div>
+
+    <div class="row align-items-center mb-5">
+        <div class="col-md-6">
+            <img src="https://vaszilijedc.hu/wp-content/uploads/2020/12/IMG_20200614_143419-01.jpeg.webp"
+                alt="IMG 20200614 143419 01" title="IMG_20200614_143419-01" style="max-width: 60%; height: auto;"
+                class="img-fluid">
+        </div>
+        <div class="col-md-6">
+            <h2>De mi az az EDC?</h2>
+            <p class="text-justify">Egy angol betűszó, amely kibontva az every day carry kifejezést takarja. Ez
+                szó szerinti fordításban azokat a holmikat jelenti, amelyeket nap mint nap
+                magunknál hordunk. A közkeletű tévhittel ellentétben nem szükséges az, hogy
+                mindig minden nap nálunk legyen: inkább egyfajta készletről, gyűjteményről
+                van szó, amelynek elemeit az adott szituációnak megfelelően váltogathatjuk.
+                Más holmikat pakolunk el, ha egy irodába megyünk dolgozni, mást, ha egy
+                építkezésen melózunk, és akkor is, amikor hétvégén rokonlátogatóba megyünk.
+            </p>
+            <p class="text-justify">Ezen a blogon főképp késekről olvashatsz, mert hozzám ezek az eszközök állnak
+                legközelebb, de szó esik néha másról is. Multiszerszámokról, táskákról,
+                egyéb kiegészítőkről. És nem csak bemutatókat készítek: ahogy már írtam,
+                sokféle aspektusa érdekel ennek a világnak.</p>
+            <p class="text-justify">Az every day carry tehát sok minden lehet. Életmód, filozófia, hobbi, vagy
+                akár egy gyűjtőszenvedély alapja. Mindegy, hogy téged melyik része érdekel,
+                remélem, találsz itt értékes olvasnivalót.</p>
+        </div>
+    </div>
+
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col mb-4">
+            <div class="card h-100 text-center">
+                <img src="https://vaszilijedc.hu/wp-content/uploads/2021/12/29366421_1622414891184377_7218577531824242688_n.jpg.webp"
+                    class="card-img-top img-fluid" alt="Bladeshop">
+                <div class="card-body">
+                    <h5 class="card-title">Bladeshop</h5>
+                    <p class="card-text">Késes webshop gyakori akciókkal és vevőbarát hozzáállással. Ha új kés kell, ne
+                        hagyd ki!</p>
+                </div>
+                <div class="card-footer bg-transparent border-0">
+                    <a href="https://www.bladeshop.hu/" target="_blank" rel="noopener" class="btn btn-dark">Megnézem</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col mb-4">
+            <div class="card h-100 text-center">
+                <img src="https://vaszilijedc.hu/wp-content/uploads/2021/12/162854478_131232062335812_8008164456523881828_n.jpg.webp"
+                    class="card-img-top img-fluid" alt="Elemlámpa blog">
+                <div class="card-body">
+                    <h5 class="card-title">Elemlámpa blog</h5>
+                    <p class="card-text">Minden, amit az elemlámpákról tudni szeretnél. Cikkek, bemutatók, illetve
+                        kuponok gyűjtőhelye.</p>
+                </div>
+                <div class="card-footer bg-transparent border-0">
+                    <a href="https://elemlampablog.hu/" target="_blank" rel="noopener" class="btn btn-dark">Megnézem</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col mb-4">
+            <div class="card h-100 text-center">
+                <img src="https://vaszilijedc.hu/wp-content/uploads/2021/12/Kesvilag.jpg.webp"
+                    class="card-img-top img-fluid" alt="Késvilág">
+                <div class="card-body">
+                    <h5 class="card-title">Késvilág</h5>
+                    <p class="card-text">Hazai bolt és webáruház, rendkívül széles termékválasztékkal. Debrecenben
+                        személyesen is válogathatsz!</p>
+                </div>
+                <div class="card-footer bg-transparent border-0">
+                    <a href="https://www.kesvilag.hu/" target="_blank" rel="noopener" class="btn btn-dark">Megnézem</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col mb-4">
+            <div class="card h-100 text-center">
+                <img src="https://vaszilijedc.hu/wp-content/uploads/2021/12/mkszoveglogo.png.webp"
+                    class="card-img-top img-fluid" alt="mkszoveglogo">
+                <div class="card-body">
+                    <h5 class="card-title">Magyar kések</h5>
+                    <p class="card-text">Webshop és közösség. Elsősorban a hazai készítők termékeivel foglalkozik,
+                        de nyitott egyéb irányokba is.</p>
+                </div>
+                <div class="card-footer bg-transparent border-0">
+                    <a href="https://www.magyarkesek.hu/" target="_blank" rel="noopener"
+                        class="btn btn-dark">Megnézem</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col mb-4">
+            <div class="card h-100 text-center">
+                <img src="https://vaszilijedc.hu/wp-content/uploads/2021/12/993918_421456207969831_2037177305_n.jpg.webp"
+                    class="card-img-top img-fluid" alt="993918 421456207969831 2037177305 n">
+                <div class="card-body">
+                    <h5 class="card-title">Késportál</h5>
+                    <p class="card-text">Magyarország legnagyobb késes tudásbázisa. Érdemes csatlakoznod a
+                        fórumhoz is!</p>
+                </div>
+                <div class="card-footer bg-transparent border-0">
+                    <a href="https://kesportal.hu/" target="_blank" rel="noopener" class="btn btn-dark">Megnézem</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col mb-4">
+            <div class="card h-100 text-center">
+                <img src="https://vaszilijedc.hu/wp-content/uploads/2021/12/ZBOSSweb_fekete_hatterrel-kicsi.jpg.webp"
+                    class="card-img-top img-fluid" alt="ZBOSSweb fekete hatterrel kicsi">
+                <div class="card-body">
+                    <h5 class="card-title">ZBOSS</h5>
+                    <p class="card-text">Kések, edc felszerelések, túra és sok egyéb. Hazai webáruház, ahol a
+                        vevők elégedettsége a legfontosabb.</p>
+                </div>
+                <div class="card-footer bg-transparent border-0">
+                    <a href="https://www.zboss.hu/" target="_blank" rel="noopener" class="btn btn-dark">Megnézem</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row my-5">
+        <div class="col">
+            <hr>
+        </div>
+    </div>
+
+    <div class="container my-5">
+        <div class="row justify-content-center text-center mb-4">
+            <div class="col-md-5 mb-3">
+                <video width="100%" height="auto" controls class="rounded shadow-sm">
+                    <source src="assets/videos/showyouthecode.mp4" type="video/mp4">
+                    A böngésző nem támogatja a videó lejátszását.
+                </video>
+            </div>
+
+            <div class="col-md-5 mb-3">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item"
+                        src="https://www.youtube.com/embed/jCZuBLD_j10?si=4j1DRBFS2Nkfegf-" allowfullscreen>
+                    </iframe>
+                </div>
+            </div>
+        </div>
+
+        <div class="row justify-content-center text-center">
+            <div class="col-md-8">
+                <h5>Elérhetőség térképen</h5>
+                <div class="embed-responsive embed-responsive-16by9 rounded">
+                    <iframe class="embed-responsive-item"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2701.575444420692!2d19.222816375448705!3d47.38120417116998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741eb8ed25e9983%3A0x1001b036eff20959!2sVencel%20Bal%C3%A1zs%20E.V.!5e0!3m2!1shu!2hu!4v1744972642054!5m2!1shu!2hu"
+                        allowfullscreen loading="lazy">
+                    </iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+    
