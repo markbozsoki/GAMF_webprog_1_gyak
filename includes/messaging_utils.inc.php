d150f23efbeb223769040ead892d09397e4ff85c
markbozsoki <bozsoki.mark@icloud.com>
2025-04-26 20:46:21 +0200

    data_access_implementations

--- includes/messaging_utils.inc.php
+++ includes/messaging_utils.inc.php
@@ -1,5 +1,7 @@
 <?php
 const MAXIMUM_MESSAGE_LENGTH = 7495;
+const MAXIMUM_SUBJECT_LENGTH = 440;
+const MAXIMUM_EMAIL_LENGTH = 250;
 
 const DEFAULT_PAGINATION_START_INDEX = 0;
 const MINIMUM_PAGINATION_PAGE_SIZE = 1;
@@ -7,6 +9,29 @@ const DEFAULT_PAGINATION_PAGE_SIZE = 10;
 const MAXIMUM_PAGINATION_PAGE_SIZE = 100;
 
 // TODO: implement param and data parsing
+
+function parse_email_address($DATA, $key = 'email') {
+    if (!isset($DATA[$key])){
+        return NULL;
+    }
+    //TODO: parsing is not implemented yet
+    $value = $DATA[$key];
+    if (strlen($value) === 0 || strlen($value) > MAXIMUM_EMAIL_LENGTH) {
+        return NULL;
+    }
+    return $value;
+}
+
+function parse_message_subject($DATA, $key = 'subject') {
+    if (!isset($DATA[$key])){
+        return NULL;
+    }
+    $value = $DATA[$key];
+    if (strlen($value) === 0 || strlen($value) > MAXIMUM_SUBJECT_LENGTH) {
+        return NULL;
+    }
+    return $value;
+}
 
 function parse_message_text($DATA, $key = 'message') {
     if (!isset($DATA[$key])){
@@ -19,15 +44,6 @@ function parse_message_text($DATA, $key = 'message') {
     return $value;
 }
 
-function parse_email_address($DATA, $key = 'email') {
-    if (!isset($DATA[$key])){
-        return NULL;
-    }
-    //TODO: parsing is not implemented yet
-    $value = $DATA[$key];
-    return $value;
-}
-
 function parse_message_id($DATA, $key = 'message_id') {
     if (!isset($DATA[$key])){
         return NULL;
@@ -37,43 +53,55 @@ function parse_message_id($DATA, $key = 'message_id') {
     return $value;
 }
 
-// TODO: implement db commonication
-
-function save_new_message($sender_id = NULL, $message_text): ?string {
-    throw new Exception("[" . __FUNCTION__ . "] - Is Not Implemented Yet");
-    
-    $insert_new_message_template = "INSERT INTO MESSAGES  VALUES (default, default, :sender_id, UNIX_TIMESTAMP(NOW()), :message_text);";
+function save_new_message($sender_id = NULL, $email_address, $subject_text, $message_text): ?string {    
+    $insert_new_message_template = "INSERT INTO MESSAGES VALUES (default, default, :sender_id, UNIX_TIMESTAMP(NOW()), :email_address, :subject_text, :message_text);";
     $insert_new_message_params = array(
         ':sender_id' => $sender_id,
+        ':email_address' => $email_address,
+        ':subject_text' => $subject_text,
         ':message_text' => $message_text,
     );
 
-    $new_message_id_query_template = "SELECT ";
+    $new_message_id_query_template = "SELECT msg_id AS new_message_id FROM MESSAGES WHERE id = :new_message_record_id;";
+    $new_message_id_params = array(
+        ':new_message_record_id' => NULL,
+    );
 
     $data_access_layer = DataAccessLayerSingleton::getInstance();
     try {
         $data_access_layer->beginTransaction();
-        
         $data_access_layer->executeCommand($insert_new_message_template, $insert_new_message_params);
         $data_access_layer->commit();
 
         $new_message_record_id = $data_access_layer->lastInsertId();
+        $new_message_id_params[':new_message_record_id'] = $new_message_record_id;
         
         $result = $data_access_layer->executeCommand($insert_user_record_template, $insert_user_record_params);
-        
+        if (!isset($result['new_message_id'])) {
+            throw new Exception("[" . __FUNCTION__ . "] - Query result does not contain 'new_message_id'!");
+        }
+        return $result['new_message_id'];
     } catch (Exception $e) {
         $data_access_layer->rollBack();
         throw $e;
     }
 }
 
-function is_message_exists($message_id) {
-    throw new Exception("[" . __FUNCTION__ . "] - Is Not Implemented Yet");
+function is_message_exists($message_id): bool {
+    $query_template = "SELECT count(username) AS message_exists FROM MESSAGES WHERE msg_id = :message_id;";
+    $params = array(':message_id' => $message_id);
 
+    $result = DataAccessLayerSingleton::getInstance()->executeCommand($query_template, $params);
+    if (!isset($result['message_exists'])) {
+        throw new Exception("[" . __FUNCTION__ . "] - Query result does not contain 'message_exists'!");
+    }
+    if ($result['message_exists'] > 1) {
+        throw new Exception("[" . __FUNCTION__ . "] - Query result indicates multiple messages by this message_id (" . $message_id . ")!");
+    }
+    return (bool) $result['message_exists'];
 }
 
 function get_paginated_messages($start_index = DEFAULT_PAGINATION_START_INDEX, $page_size = DEFAULT_PAGINATION_PAGE_SIZE) {
-    throw new Exception("[" . __FUNCTION__ . "] - Is Not Implemented Yet");
     if ($start_index < DEFAULT_PAGINATION_START) {
         $start_index = DEFAULT_PAGINATION_START;
     }
@@ -84,7 +112,7 @@ function get_paginated_messages($start_index = DEFAULT_PAGINATION_START_INDEX, $
         $page_size = MAXIMUM_PAGINATION_PAGE_SIZE;
     }
 
-    $query_template = "SELECT msg_id, sender_id, from_unixtime(sent_at), msg_text FROM MESSAGES ORDER BY MESSAGES.sent_at DESC LIMIT :page_size OFFSET :start_index;";
+    $query_template = "SELECT * FROM MESSAGES ORDER BY MESSAGES.sent_at DESC LIMIT :page_size OFFSET :start_index;";
     $params = array(
         ':page_size' => $page_size,
         ':start_index' => $start_index,
@@ -99,8 +127,6 @@ function get_paginated_messages($start_index = DEFAULT_PAGINATION_START_INDEX, $
 }
 
 function extend_message_with_user_detail($message): array {
-    throw new Exception("[" . __FUNCTION__ . "] - Is Not Implemented Yet");
-
     if (!isset($message['sender_id'])) {
         throw new Exception("[" . __FUNCTION__ . "] - Missing sender_id from message: " . var_dump($message));
     }
@@ -111,12 +137,22 @@ function extend_message_with_user_detail($message): array {
         return $messsage;
     }
 
-    $query_template = "SELECT surname, forename FROM DETAILS WHERE id IN (SELECT detail_id FROM USERS WHERE id = :user_id);";
+    $query_template = "SELECT username, surname, forename FROM USER_DETAILS WHERE user_id = :user_id;";
     $params = array(':user_id' => $message['sender_id']);
 
     $result = DataAccessLayerSingleton::getInstance()->executeCommand($query_template, $params);
+    if (!isset($result['username'])) {
+        throw new Exception("[" . __FUNCTION__ . "] - Query result does not contain 'username'!");
+    }
+    if (!isset($result['surname'])) {
+        throw new Exception("[" . __FUNCTION__ . "] - Query result does not contain 'surname'!");
+    }
+    if (!isset($result['forename'])) {
+        throw new Exception("[" . __FUNCTION__ . "] - Query result does not contain 'forename'!");
+    }
 
-    $message[$user_detail_key] = $result[''] . " " . $result[''] . " (" . $result[''] . ")";
+    $message[$user_detail_key] = $result['surname'] . " " . $result['forename'] . " (" . $result['username'] . ")";
+
     return $messsage;
 }
 
