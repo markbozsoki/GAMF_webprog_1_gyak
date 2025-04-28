787c53f69d97b6f8f023cb3c80b4aac6d627f5e8
markbozsoki <bozsoki.mark@icloud.com>
2025-04-29 00:04:56 +0200

    overprotect_session_based_message_view_access

--- includes/session.inc.php
+++ includes/session.inc.php
@@ -22,7 +22,13 @@ function clear_message_auth_session() {
 }
 
 function _generate_message_id_auth_key($message_id): string {
-    return hash('md2', 'salt' . $message_id . 'pepper');
+    if (getenv('MSG_ID_SALT') == NULL || getenv('MSG_ID_PEPPER') == NULL) {
+        throw new Exception('MSG_ID_SALT and MSG_ID_PEPPER must be set!');
+    }
+    $_search = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    $replace = ['i', "'", 'm', 'a', 't', 'e', '@', 'p', 'o', '!']; // RCF2324 based string suffle
+    $shuffled_message_id = str_replace($_search, $replace, $message_id);
+    return hash('md5', getenv('MSG_ID_SALT') . $shuffled_message_id . getenv('MSG_ID_PEPPER'));
 }
 
 function set_message_auth_session($message_id) {
