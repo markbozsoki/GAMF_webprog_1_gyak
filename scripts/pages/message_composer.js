8988b2ba57c933966b2401d83a416569919c1371
MoSzabina <moszabina@gmail.com>
2025-04-29 15:41:07 +0200

    4. feladat Üzenetküldés űrlap ellenőrzése kliens oldalon

---
+++ scripts/pages/message_composer.js
@@ -0,0 +1,66 @@
+(function() {
+  'use strict';
+
+  window.addEventListener('load', function() {
+    var form = document.getElementById('messageComposerForm');
+
+    form.addEventListener('submit', function(event) {
+      var emailField = form.querySelector('[name="email"]');
+      var subjectField = form.querySelector('[name="subject"]');
+      var bodyField = form.querySelector('[name="body"]');
+
+      var valid = true;
+
+      var emailValue = emailField.value;
+      var subjectValue = subjectField.value;
+      var bodyValue = bodyField.value;
+
+      var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+
+      
+      if (emailValue === "") {
+        emailField.setCustomValidity("Az e-mail cím nem lehet üres.");
+        valid = false;
+      } else if (emailValue.length > 230) {
+        emailField.setCustomValidity("Az e-mail cím maximum 230 karakter lehet.");
+        valid = false;
+      } else if (!emailPattern.test(emailValue)) {
+        emailField.setCustomValidity("Nem megfelelő formátum.");
+        valid = false;
+      } else {
+        emailField.setCustomValidity("");
+      }
+
+      if (subjectValue.trim() === "") {
+        subjectField.setCustomValidity("A tárgy mező nem lehet üres.");
+        valid = false;
+      } else if (subjectValue.length > 400) {
+        subjectField.setCustomValidity("A tárgy maximum 400 karakter lehet.");
+        valid = false;
+      } else {
+        subjectField.setCustomValidity("");
+      }
+
+      if (bodyValue.trim() === "") {
+        bodyField.setCustomValidity("Az üzenet mező nem lehet üres.");
+        valid = false;
+      } else if (bodyValue.length > 7000) {
+        bodyField.setCustomValidity("Az üzenet maximum 7000 karakter lehet.");
+        valid = false;
+      } else {
+        bodyField.setCustomValidity("");
+      }
+
+      //prevent sending if error detected
+      if (!form.checkValidity() || !valid) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+
+      form.classList.add('was-validated');
+
+    }, false);
+
+  }, false);
+
+})();
