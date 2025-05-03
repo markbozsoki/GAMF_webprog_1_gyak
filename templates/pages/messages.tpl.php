11ad968bd484f6a2db69d9880475189e7e0be55d
markbozsoki <bozsoki.mark@icloud.com>
2025-05-03 17:49:44 +0200

    add_paginators_to_the_bpttom

--- templates/pages/messages.tpl.php
+++ templates/pages/messages.tpl.php
@@ -19,48 +19,67 @@
     </nav>
 </div>
 
-<div>
-    <table>
+<div class="m-1">
+    <table class="table table-striped">
         <thead>
             <tr>
-                <th>#</th>
-                <th>Küldve</th>
-                <th>Küldő</th>
-                <th>Email</th>
-                <th>Tárgy</th>
-                <th>Üzenet</th>
-                <th>Id</th>
+                <th scope="col">#</th>
+                <th scope="col">Küldve</th>
+                <th scope="col">Küldő</th>
+                <th scope="col">Email</th>
+                <th scope="col">Tárgy</th>
+                <th scope="col">Üzenet</th>
+                <th scope="col">Link</th>
             </tr>
         </thead>
 
         <tbody>
             <?php foreach ($PAGINATED_MESSAGE_DATA['data'] as $key => $message_data) { ?>
             <tr>
-                <th>
+                <th scope="row">
                     <?php echo $PAGINATED_MESSAGE_DATA['start'] + (int)$key + 1 ?>
                 </th>
-                <th>
+                <td>
                     <?php echo $message_data['sent_at'] ?>
-                </th>
-                <th>
+                </td>
+                <td>
                     <?php echo $message_data['user_detail'] ?>
-                </th>
-                <th>
+                </td>
+                <td>
                     <?php echo $message_data['email_address'] ?>
-                </th>
-                <th>
+                </td>
+                <td>
                     <?php echo substr($message_data['subject'], 0, 20) . '...' ?>
-                </th>
-                <th>
+                </td>
+                <td>
                     <?php echo substr($message_data['body'], 0, 20) . '...' ?>
-                </th>
-                <th>
+                </td>
+                <td>
                     <a href="<?= $PAGINATED_MESSAGE_DATA['link']['current'] ?>&message=<?= $message_data['message_id'] ?>">
                         Megtekintés
                     </a>
-                </th>
+                </td>
             </tr>
             <?php } ?>
         </tbody>
     </table>
+</div>
+
+<div>
+    <nav>
+        <ul class="pagination">
+            <li class="page-item <?= $PAGINATED_MESSAGE_DATA['start'] === 0 ? ' disabled' : '' ?>">
+                <?php $prev_page_link = $PAGINATED_MESSAGE_DATA['link']['previous']; ?>
+                <a class="page-link" href="<?= $prev_page_link ?>">
+                    Előző oldal
+                </a>
+            </li>
+            <li class="page-item <?= count($PAGINATED_MESSAGE_DATA['data']) < $PAGINATED_MESSAGE_DATA['size'] ? ' disabled' : '' ?>">
+                <?php $next_page_link = $PAGINATED_MESSAGE_DATA['link']['next']; ?>
+                <a class="page-link" href="<?= $next_page_link ?>">
+                    Következő oldal
+                </a>
+            </li>
+        </ul>
+    </nav>
 </div>
