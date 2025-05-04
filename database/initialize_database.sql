75bc097c7228df762b51860e7c8d99b5de0656e5
Márk Bozsóki <83731022+markbozsoki@users.noreply.github.com>
2025-05-04 16:02:08 +0200

    mariaDB 10.4 -> mySQL 5.7 kompatibilitási probléma feloldása  (#23)

--- database/initialize_database.sql
+++ database/initialize_database.sql
@@ -1,16 +1,16 @@
--- DEBUG USER FOR CONNECTION
+---------- DEBUG USER FOR CONNECTION ----------
 CREATE USER IF NOT EXISTS 'debug_user'@'localhost' IDENTIFIED BY 'password';
 GRANT SELECT, INSERT, UPDATE ON *.* TO 'debug_user'@'localhost' 
 REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 3600 MAX_CONNECTIONS_PER_HOUR 3600 MAX_UPDATES_PER_HOUR 3600 MAX_USER_CONNECTIONS 3600;
 GRANT SELECT, INSERT, UPDATE ON `knives\_database`.* TO 'debug_user'@'localhost';
 
--- DATABASE
+---------- DATABASE ----------
 CREATE DATABASE IF NOT EXISTS `knives_database`
 CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
 
 USE `knives_database`;
 
--- TABLES
+---------- CREATE TABLES ----------
 CREATE TABLE IF NOT EXISTS `ACCESS` (
   `id` int(5) unsigned NOT NULL auto_increment,
   `created_at` int(10) NOT NULL,
@@ -39,7 +39,8 @@ CREATE TABLE IF NOT EXISTS `USERS` (
 
 CREATE TABLE IF NOT EXISTS `MESSAGES` (
   `id` int(5) unsigned NOT NULL auto_increment,
-  `msg_id` VARCHAR(16) default CONCAT("msg", SUBSTRING(UUID(), 1, 8), SUBSTRING(UUID(), 2, 6)) unique,
+  -- `msg_id` VARCHAR(16) default CONCAT("msg", SUBSTRING(UUID(), 1, 8), SUBSTRING(UUID(), 2, 6)) unique,
+  `msg_id` VARCHAR(16) NOT NULL unique,
   `sender_id` int(5) default NULL,
   `sent_at` int(10) NOT NULL,
   `email_address` varchar(255) NOT NULL,
@@ -48,11 +49,13 @@ CREATE TABLE IF NOT EXISTS `MESSAGES` (
   PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 
--- VIEWS
+---------- CREATE VIEWS ----------
 CREATE VIEW IF NOT EXISTS `USERNAMES` AS
+-- CREATE VIEW `USERNAMES` AS
 SELECT username FROM USERS;
 
 CREATE VIEW IF NOT EXISTS `USER_DETAILS` AS
+-- CREATE VIEW `USER_DETAILS` AS
 SELECT USERS.id AS user_id,
        USERS.username AS username, 
        DETAILS.surname AS surname, 
@@ -60,6 +63,7 @@ SELECT USERS.id AS user_id,
 FROM USERS LEFT JOIN DETAILS ON USERS.detail_id = DETAILS.id;
 
 CREATE VIEW IF NOT EXISTS `USER_LOGINS` AS
+-- CREATE VIEW `USER_LOGINS` AS
 SELECT USERS.username AS username, 
        from_unixtime(ACCESS.last_logged_in) AS last_logged_in,
        from_unixtime(ACCESS.created_at) AS access_created_at,
@@ -71,6 +75,7 @@ FROM USERS LEFT JOIN ACCESS ON USERS.access_id = ACCESS.id
 ORDER BY logged_in_epoch DESC, access_crtd_epoch DESC, user_crtd_epoch DESC;
 
 CREATE VIEW IF NOT EXISTS `ORPHAN_ACCESS_RECORDS` AS
+-- CREATE VIEW `ORPHAN_ACCESS_RECORDS` AS
 SELECT * 
 FROM ACCESS 
 WHERE id NOT IN (
@@ -79,6 +84,7 @@ WHERE id NOT IN (
   );
 
 CREATE VIEW IF NOT EXISTS `ORPHAN_DETAILS_RECORDS` AS
+-- CREATE VIEW `ORPHAN_DETAILS_RECORDS` AS
 SELECT * 
 FROM DETAILS 
 WHERE id NOT IN (
@@ -86,7 +92,7 @@ WHERE id NOT IN (
   FROM USERS
   );
 
--- REGISTER ADMIN USER
+---------- REGISTER ADMIN USER ----------
 SET @username = 'admin';
 SET @password = 'admin';
 SET @surname = 'The';
@@ -97,5 +103,5 @@ INSERT IGNORE INTO `DETAILS` (`id`, `surname`, `forename`) VALUES (default, @sur
 SET @details_last_id = LAST_INSERT_ID();
 INSERT IGNORE INTO `USERS` (`id`, `username`, `created_at`, `access_id`, `detail_id`) VALUES (default, @username, UNIX_TIMESTAMP(NOW()), @access_last_id, @details_last_id);
 
--- ADD TEST MESSAGE: ?message=test
-INSERT IGNORE INTO `messages` (`id`, `msg_id`, `sender_id`, `sent_at`, `email_address`, `subject`, `msg_text`) VALUES (default, "test", default, UNIX_TIMESTAMP(NOW() - INTERVAL 5 MINUTE), "'guest.user@test.com'", TO_BASE64("Test Subject"), TO_BASE64("Hello,\nI hope this email finds you well!\n\n 0===}::::::::::::::> \n\nBye"));
+---------- ADD TEST MESSAGE: ?message=test ----------
+INSERT IGNORE INTO `MESSAGES` (`id`, `msg_id`, `sender_id`, `sent_at`, `email_address`, `subject`, `msg_text`) VALUES (default, "test", default, UNIX_TIMESTAMP(NOW() - INTERVAL 5 MINUTE), "'guest.user@test.com'", TO_BASE64("Test Subject"), TO_BASE64("Hello,\nI hope this email finds you well!\n\n 0===}::::::::::::::> \n\nBye"));
