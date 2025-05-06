67391ec55b5676b5f31c78eba6839ca276aba651
markbozsoki <bozsoki.mark@icloud.com>
2025-05-06 23:09:27 +0200

    remove_message_id_suffler

--- includes/session.inc.php
+++ includes/session.inc.php
@@ -25,10 +25,7 @@ function _generate_message_id_auth_key($message_id): string {
     if (getenv('MSG_ID_SALT') == NULL || getenv('MSG_ID_PEPPER') == NULL) {
         throw new Exception('MSG_ID_SALT and MSG_ID_PEPPER must be set!');
     }
-    $_search = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    $replace = ['i', "'", 'm', 'a', 't', 'e', '@', 'p', 'o', '!']; // RCF2324 based string suffle
-    $shuffled_message_id = str_replace($_search, $replace, $message_id);
-    return hash('md5', getenv('MSG_ID_SALT') . $shuffled_message_id . getenv('MSG_ID_PEPPER'));
+    return hash('md5', getenv('MSG_ID_SALT') . $message_id . getenv('MSG_ID_PEPPER'));
 }
 
 function set_message_auth_session($message_id) {
